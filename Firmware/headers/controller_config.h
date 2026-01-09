63eb920771456a55375afd28d61e64fa14a5a2b6
miczki-17 <miczkipap@gmail.com>
2026-01-09 19:34:45 +0100

    Update controller_config.h

--- Firmware/headers/controller_config.h
+++ Firmware/headers/controller_config.h
@@ -1,1 +1,51 @@
+#ifndef CONTROLLER_CONFIG_H
+#define CONTROLLER_CONFIG_H
 
+// bitmask for buttons
+
+#define OPTIONS 0x20
+#define SHARE   0x10
+
+
+#define Y_BUTTON 0x08
+#define X_BUTTON 0x04
+#define B_BUTTON 0x02
+#define A_BUTTON 0x01
+
+
+#define R2 0x0080
+#define L2 0x0040
+#define R1 0x0020
+#define L1 0x0010
+
+
+#define RIGHT_BUTTON 0x04
+#define LEFT_BUTTON  0x08
+#define UP_BUTTON    0x01
+#define DOWN_BUTTON  0x02
+
+
+#define RIGHT_DRAG 0x0200
+#define LEFT_DRAG  0x0100
+
+
+#define MISC_OPTIONS 0x04
+#define MISC_SHARE   0x02
+#define MISC_PS		 0x01
+
+
+// colors for ps4 controller
+
+#define RED_COLOR 0
+#define GREEN_COLOR 1
+#define BLUE_COLOR 2
+
+
+
+// drags zones
+
+#define LEFT_DRAG_ZONE	36
+#define RIGHT_DRAG_ZONE 36
+
+
+#endif
