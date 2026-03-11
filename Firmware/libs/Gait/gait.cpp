a87756779c67dc1082c26b8d2b5f3718a585fb17
miczki-17 <miczkipap@gmail.com>
2026-03-11 17:19:21 +0100

    Update gait.cpp

--- Firmware/libs/Gait/gait.cpp
+++ Firmware/libs/Gait/gait.cpp
@@ -1,684 +1,139 @@
-#include "gait.h"
+#include "gait2.h"
+#include <math.h>
 
-#include "math.h"
+#define PI 3.14159265359f
 
-#define RAD_TO_DEG (180 / 3.14159)
-
-
-Gait::Gait(leg* legs, figure& bodyObj) {
-	_legs = legs;
-	_body = &bodyObj;
-
-	direction = WAIT;
+Gait2::Gait2(leg* legs, figure& body) {
+    _legs = legs;
+    _body = &body;
 }
 
-Gait::~Gait() {
-	
+void Gait2::resetToDefault() {
+    for (int i = 0; i < 4; i++) {
+        _legs[i].realfoot = _legs[i].defaultfoot;
+    }
 }
 
-
-
-
-
-void Gait::nextStep() {
-	switch (this->direction) {
-	case STRAIGHT:
-		if (step.straight == STEP2) {
-			step.straight = STEP1;
-			break;
-		}
-		step.straight <<= 0x01;
-		break;
-	case RIGHT:
-		if (step.right == STEP2) {
-			step.right = STEP1;
-			break;
-		}
-		step.right <<= 0x01;
-		break;
-	case LEFT:
-		if (step.left == STEP2) {
-			step.left = STEP1;
-			break;
-		}
-		step.left <<= 0x01;
-		break;
-	case BACK:
-		if (step.back == STEP2) {
-			step.back = STEP1;
-			break;
-		}
-		step.back <<= 0x01;
-		break;
-	case RIGHT_ROTATION:
-		/*if (step.right_rotation == STEP2) {
-			step.right_rotation = STEP1;
-			break;
-		}
-		step.right_rotation <<= 0x01;*/
-		break;
-	case LEFT_ROTATION:
-		/*if (step.left_rotation == STEP2) {
-			step.left_rotation == STEP1;
-			break;
-		}
-		step.left_rotation <<= 0x01;*/
-		break;
-	default:
-		break;
-	}
+void Gait2::setVelocity(float vx, float vy) {
+    _vx = vx;
+    _vy = vy;
 }
 
-
-// analog
-
-void Gait::analogNextStep() {
-	if (this->current_analog_step == STEP1)
-		current_analog_step = STEP2;
-	else
-		current_analog_step = STEP1;
+void Gait2::setVelocity(float vx, float vy, float omega) {
+    _vx = vx;
+    _vy = vy;
+    _omega = omega;
 }
 
+void Gait2::update(float dt) {
+
+    // Low-pass filtering predkosci
+    _filteredVx += _alphaFilter * (_vx - _filteredVx);
+    _filteredVy += _alphaFilter * (_vy - _filteredVy);
+
+    _filteredOmega += _alphaFilter * (_omega - _filteredOmega);
+    _yaw += _filteredOmega * dt;
+
+    float velocityMagnitude = sqrt(_filteredVx * _filteredVx + _filteredVy * _filteredVy);
+    float motionMagnitude = velocityMagnitude + abs(_filteredOmega * 10.0f);
+
+    // Deadband - robot staje, gdy ruch jest ponizej progu
+    if (motionMagnitude < 0.1f) {
+        resetToDefault();
+        return;
+    }
+
+    // --- 1. SKALOWANIE WSPOLCZYNNIKA PREDKOSCI ---
+    float translationRatio = velocityMagnitude / _maxVelocity;
+    float rotationRatio = abs(_filteredOmega) / _maxOmega;
+
+    float speedRatio = translationRatio + rotationRatio;
+    if (speedRatio > 1.0f) {
+        speedRatio = 1.0f;
+    }
+
+    // --- 2. FILTROWANA WYSOKOSC KROKU ---
+    _targetStepHeight = _minStepHeight + (_maxStepHeight - _minStepHeight) * speedRatio;
+    _currentStepHeight += _stepHeightAlpha * (_targetStepHeight - _currentStepHeight);
+
+    // --- 3. DYNAMICZNA CZESTOTLIWOSC KROKU ---
+    _currentStepFrequency = _minStepFrequency + (_maxStepFrequency - _minStepFrequency) * speedRatio;
+
+    // --- 4. DYNAMICZNE OBNIZANIE TULOWIA ---
+    _currentBodyDrop = _maxBodyDrop * speedRatio;
 
 
+    // --- AKTUALIZACJA FAZY I KROKU ---
+    float stepLength = velocityMagnitude / _currentStepFrequency;
 
+    _phase += dt * _currentStepFrequency;
 
+    if (_phase > 1.0f)
+        _phase -= 1.0f;
 
+    for (int i = 0; i < 4; i++) {
+        float legPhase = _phase + legPhaseOffset[i];
+        if (legPhase > 1.0f)
+            legPhase -= 1.0f;
 
-
-
-void Gait::setDirection(const int8_t direction) {
-	this->direction = direction;
+        computeFootTrajectory(i, legPhase);
+    }
 }
 
-
-void Gait::move() {
-	switch (this->direction) {
-	case STRAIGHT:
-		switch (step.straight) {
-		case STEP1:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.y++;
-				legs[LEGLH].realfoot.y++;
-				legs[LEGRH].realfoot.y--;
-				legs[LEGLF].realfoot.y--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.y++;
-				legs[LEGRH].realfoot.y++;
-				legs[LEGLH].realfoot.y--;
-				legs[LEGRF].realfoot.y--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		case STEP2:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.y++;
-				legs[LEGRH].realfoot.y++;
-				legs[LEGLH].realfoot.y--;
-				legs[LEGRF].realfoot.y--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.y++;
-				legs[LEGLH].realfoot.y++;
-				legs[LEGRH].realfoot.y--;
-				legs[LEGLF].realfoot.y--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		default:
-			break;
-		}
-		break;
-
-
-
-
-	case RIGHT:
-		switch (step.right) {
-		case STEP1:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.x++;
-				legs[LEGLH].realfoot.x++;
-				legs[LEGRH].realfoot.x--;
-				legs[LEGLF].realfoot.x--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.x++;
-				legs[LEGRH].realfoot.x++;
-				legs[LEGLH].realfoot.x--;
-				legs[LEGRF].realfoot.x--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		case STEP2:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.x++;
-				legs[LEGRH].realfoot.x++;
-				legs[LEGLH].realfoot.x--;
-				legs[LEGRF].realfoot.x--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.x++;
-				legs[LEGLH].realfoot.x++;
-				legs[LEGRH].realfoot.x--;
-				legs[LEGLF].realfoot.x--;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		default:
-			break;
-		}
-		break;
-
-
-
-
-
-	case LEFT:
-		switch (step.left) {
-		case STEP1:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.x--;
-				legs[LEGLH].realfoot.x--;
-				legs[LEGRH].realfoot.x++;
-				legs[LEGLF].realfoot.x++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.x--;
-				legs[LEGRH].realfoot.x--;
-				legs[LEGLH].realfoot.x++;
-				legs[LEGRF].realfoot.x++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		case STEP2:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.x--;
-				legs[LEGRH].realfoot.x--;
-				legs[LEGLH].realfoot.x++;
-				legs[LEGRF].realfoot.x++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.x--;
-				legs[LEGLH].realfoot.x--;
-				legs[LEGRH].realfoot.x++;
-				legs[LEGLF].realfoot.x++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		}
-		break;
-
-
-
-
-	case BACK:
-		switch (step.back) {
-		case STEP1:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.y--;
-				legs[LEGLH].realfoot.y--;
-				legs[LEGRH].realfoot.y++;
-				legs[LEGLF].realfoot.y++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.y--;
-				legs[LEGRH].realfoot.y--;
-				legs[LEGLH].realfoot.y++;
-				legs[LEGRF].realfoot.y++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		case STEP2:
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGLF].realfoot.z++;
-				legs[LEGRH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGLF].realfoot.y--;
-				legs[LEGRH].realfoot.y--;
-				legs[LEGLH].realfoot.y++;
-				legs[LEGRF].realfoot.y++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGLF].realfoot.z--;
-				legs[LEGRH].realfoot.z--;
-				delay(1);
-			}
-
-			for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-				legs[LEGRF].realfoot.z++;
-				legs[LEGLH].realfoot.z++;
-				delay(1);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DISTANCE; i++) {
-				legs[LEGRF].realfoot.y--;
-				legs[LEGLH].realfoot.y--;
-				legs[LEGRH].realfoot.y++;
-				legs[LEGLF].realfoot.y++;
-				delay(2);
-			}
-			for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-				legs[LEGRF].realfoot.z--;
-				legs[LEGLH].realfoot.z--;
-				delay(1);
-			}
-			break;
-		}
-		break;
-
-	case RIGHT_ROTATION:
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGRF].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGRF].realfoot.x++;
-			legs[LEGRF].realfoot.y--;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGRF].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGLF].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGLF].realfoot.x++;
-			legs[LEGLF].realfoot.y++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGLF].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGRH].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGRH].realfoot.x--;
-			legs[LEGRH].realfoot.y--;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGRH].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGLH].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGLH].realfoot.x--;
-			legs[LEGLH].realfoot.y++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGLH].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGRF].realfoot.x--;
-			legs[LEGRF].realfoot.y++;
-
-			legs[LEGLF].realfoot.x--;
-			legs[LEGLF].realfoot.y--;
-
-			legs[LEGRH].realfoot.x++;
-			legs[LEGRH].realfoot.y++;
-
-			legs[LEGLH].realfoot.x++;
-			legs[LEGLH].realfoot.y--;
-
-			delay(1);
-		}
-		break;
-
-
-
-	case LEFT_ROTATION:
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGRF].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_LEFT_ROTATION_DISTANCE; i++) {
-			legs[LEGRF].realfoot.x--;
-			legs[LEGRF].realfoot.y++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGRF].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGLF].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_LEFT_ROTATION_DISTANCE; i++) {
-			legs[LEGLF].realfoot.x--;
-			legs[LEGLF].realfoot.y--;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGLF].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGRH].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_LEFT_ROTATION_DISTANCE; i++) {
-			legs[LEGRH].realfoot.x++;
-			legs[LEGRH].realfoot.y++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGRH].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_UP; i++) {
-			legs[LEGLH].realfoot.z++;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_LEFT_ROTATION_DISTANCE; i++) {
-			legs[LEGLH].realfoot.x++;
-			legs[LEGLH].realfoot.y--;
-			delay(1);
-		}
-		for (int8_t i = 0; i < GAIT_LEG_DOWN; i++) {
-			legs[LEGLH].realfoot.z--;
-			delay(1);
-		}
-
-
-		for (int8_t i = 0; i < GAIT_LEG_RIGHT_ROTATION_DISTANCE; i++) {
-			legs[LEGRF].realfoot.x++;
-			legs[LEGRF].realfoot.y--;
-
-			legs[LEGLF].realfoot.x++;
-			legs[LEGLF].realfoot.y++;
-
-			legs[LEGRH].realfoot.x--;
-			legs[LEGRH].realfoot.y--;
-
-			legs[LEGLH].realfoot.x--;
-			legs[LEGLH].realfoot.y++;
-
-			delay(1);
-		}
-		break;
-	}
-
-	this->nextStep();
-}
-
-void Gait::attackMove(void) {
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x--;
-		this->_body->position.y--;
-		delay(8);
-	}
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x++;
-		this->_body->position.y++;
-		delay(8);
-	}
-
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x++;
-		this->_body->position.y++;
-		delay(8);
-	}
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x--;
-		this->_body->position.y--;
-		delay(8);
-	}
-
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x++;
-		this->_body->position.y--;
-		delay(8);
-	}
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x--;
-		this->_body->position.y++;
-		delay(8);
-	}
-
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x--;
-		this->_body->position.y++;
-		delay(8);
-	}
-	for (int8_t i = 0; i < 40; i++) {
-		this->_body->position.x++;
-		this->_body->position.y--;
-		delay(8);
-	}
-}
-
-
-
-
-
-// analog move
-
-void Gait::analogMove(int x, int y) {
-
-	double alpha = atan(static_cast<double>(y) / x);
-
-	double x_trim = 40 * cos(alpha);
-	double y_trim = 40 * sin(alpha);
-
-	switch (this->current_analog_step) {
-	case STEP1_ANALOG:
-		
-		break;
-
-	case STEP2_ANALOG:
-		
-		break;
-
-	default:
-		break;
-	}
-
-	this->analogNextStep();
-}
-
-
-
-
-
-
-
-
-
-void Gait::calculateCenterOfMass() {
-	
-}
-
-
-
-
-
-
-
-int8_t Gait::getStep() {
-	return 0;
+void Gait2::computeFootTrajectory(int legIndex, float phase)
+{
+    // 1. Pozycja bazowa stopy (wektor R od ?rodka robota)
+    float footBaseX = _legs[legIndex].defaultfoot.x;
+    float footBaseY = _legs[legIndex].defaultfoot.y;
+
+    // 2. Wektor pr?dko?ci dla TEJ KONKRETNEJ STOPY
+    float v_foot_x = _filteredVx + (_filteredOmega * footBaseY * rotationGain);
+    float v_foot_y = _filteredVy - (_filteredOmega * footBaseX * rotationGain);
+
+    // 3. Obliczenie d?ugo?ci kroku dla tej stopy z u?yciem DYNAMICZNEJ cz?stotliwo?ci
+    float stepLengthX = v_foot_x / _currentStepFrequency;
+    float stepLengthY = v_foot_y / _currentStepFrequency;
+
+    // 4. Parametry kroku
+    float moveX = 0.0f;
+    float moveY = 0.0f;
+    float dz = 0.0f;
+
+    // STANCE / SWING
+    if (phase < 0.5f)
+    {
+        // STANCE (noga na ziemi)
+        float t = phase / 0.5f;     // 0 -> 1
+        moveX = -stepLengthX * (t - 0.5f);
+        moveY = -stepLengthY * (t - 0.5f);
+        dz = 0.0f;
+    }
+    else
+    {
+        // SWING (noga w powietrzu)
+        float t = (phase - 0.5f) / 0.5f;   // 0 -> 1
+        moveX = stepLengthX * (t - 0.5f);
+        moveY = stepLengthY * (t - 0.5f);
+
+        // podnoszenie nogi po sinusoidzie - uzywamy ZMIENNEJ wysoko?ci
+        dz = _currentStepHeight * sin(t * PI);
+    }
+
+    float finalMoveX = moveX;
+    float finalMoveY = moveY;
+
+    // 5. OBLICZANIE POCHYLENIA TULOWIA (PITCH & ROLL)
+    // Offset uzalezniamy od predkosci na danej osi i fizycznego polozenia nogi wzgledem srodka
+    float pitchOffset = footBaseX * (_filteredVx / _maxVelocity) * _pitchMultiplier;
+    float rollOffset = footBaseY * (_filteredVy / _maxVelocity) * _rollMultiplier;
+
+    // 6. FINALNA POZYCJA STOPY
+    _legs[legIndex].realfoot.x = footBaseX + finalMoveX;
+    _legs[legIndex].realfoot.y = footBaseY + finalMoveY;
+
+    // Z-Offset: 
+    // Dodajemy wysokosc kroczenia (dz) 
+    // Dodajemy obni?anie tulowia (BodyDrop) 
+    // Dodajemy pochylenie na odpowiednich osiach (pitch/roll)
+    _legs[legIndex].realfoot.z = _legs[legIndex].defaultfoot.z + dz + _currentBodyDrop + pitchOffset + rollOffset;
 }
