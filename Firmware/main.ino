1127d8362e65c0f4c03e2d1ca54df7aa445234d3
miczki-17 <miczkipap@gmail.com>
2026-03-11 17:17:01 +0100

    Update main.ino

--- Firmware/main.ino
+++ Firmware/main.ino
@@ -7,50 +7,51 @@
 #include "libs/IK/IK.h"
 #include "libs/IK/IK.cpp"
 
-#include "libs/Gait/gait.h"
-#include "libs/Gait/gait.cpp"
-
-
-
+#include "libs/Gait/gait2.h"
+#include "libs/Gait/gait2.cpp"
 
 #if PWM_CONTROLLER == ESP32
 #include <ESP32_ISR_Servo.h>
 #endif
 
-
-
-
 #include <Bluepad32.h>
 
+// ==========================================
+// --- MPU6050 KONFIGURACJA I ZMIENNE ---
+// ==========================================
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+
+// PARAMETRY DO REGULACJI:
+float mpuAlpha = 0.05f;            // Współczynnik filtra dolnoprzepustowego
+float mpuCompensationGain = -1.6f; // Siła i kierunek kompensacji
+
+float targetPitch = 0.0f;
+float targetRoll = 0.0f;
+float currentPitch = 0.0f;
+float currentRoll = 0.0f;
+bool mpuReady = false;             // Blokuje wyginanie robota zanim wstanie
+// ==========================================
+
 ControllerPtr myControllers[BP32_MAX_GAMEPADS];
-
-
-
-
 
 figure body = {
   {0, 0, 0},
   {0, 0, 0}
 };
 
-
-Gait gait(legs, body);
-
+Gait2 gait(legs, body);
 
 IK ikLegLF(legs[LEGLF], body);
 IK ikLegRF(legs[LEGRF], body);
 IK ikLegLH(legs[LEGLH], body);
 IK ikLegRH(legs[LEGRH], body);
 
-
 iksolver legSolve[LEGS_NUM];
 
-
-
 TaskHandle_t ServiceTask;
-
-
-
 
 typedef unsigned long ul;
 
@@ -62,18 +63,9 @@ ul serviceCurrentMicros;
 ul serviceLastMicros = 0;
 ul serviceElapsedMicros;
 
-
-//ul controllerCurrentMillis;
-//ul controllerLastMillis = 0;
-//ul controllerElapsedMillis;
-
-
 bool mainLoopIsReady = false;
 bool serviceLoopIsReady = false;
-
 bool isControllerConnected = false;
-
-
 
 void setup() {
 #ifdef BAUD
@@ -90,11 +82,22 @@ void setup() {
   Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 #endif
 
+  // --- START MPU6050 BEZ KALIBRACJI ---
+  Wire.begin();
+  byte status = mpu.begin();
+#ifdef BAUD
+  if (status == 0) {
+    Serial.println(F("MPU6050 podlaczone. Czekam na pada, zeby wstanac..."));
+  } else {
+    Serial.print(F("Blad komunikacji MPU6050: "));
+    Serial.println(status);
+  }
+#endif
+  // ------------------------------------
+
   BP32.setup(&onConnectedController, &onDisconnectedController);
   BP32.enableVirtualDevice(false);
-
   initServos();
-
 
   xTaskCreatePinnedToCore(
     serviceLoop,
@@ -105,14 +108,36 @@ void setup() {
     &ServiceTask,
     0);
 
-
     mainLoopIsReady = true;
 }
 
-void loop() {                 // core 1
+void loop() {
   setBasicPositions();
 
   while (mainLoopIsReady) {
+    
+    // --- 1. AKTUALIZACJA I FILTROWANIE MPU ---
+    mpu.update();
+    
+    if (mpuReady) {
+      // 1. Pobieramy poprawnie dane z MPU (X to prawo/lewo, Y to przód/tył)
+      targetRoll = constrain(mpu.getAngleX(), -35.0f, 35.0f);
+      targetPitch = constrain(mpu.getAngleY(), -35.0f, 35.0f);
+      
+      // 2. Filtr dolnoprzepustowy (LPF)
+      currentRoll += mpuAlpha * (targetRoll - currentRoll);
+      currentPitch += mpuAlpha * (targetPitch - currentPitch);
+
+      // 3. --- KROSOWANIE OSI DO KINEMATYKI ---
+      // Zamieniamy zmienne miejscami przy wstrzykiwaniu do IK!
+      body.orientation.pitch = currentRoll * mpuCompensationGain; 
+      body.orientation.roll = currentPitch * mpuCompensationGain; 
+    } else {
+      // Robot jeszcze nie wstał – blokujemy kąty na 0
+      body.orientation.pitch = 0.0f;
+      body.orientation.roll = 0.0f;
+    }
+    // -----------------------------------------
 
     legSolve[LEGLF] = ikLegLF.solve();
     legSolve[LEGRF] = ikLegRF.solve();
@@ -122,38 +147,58 @@ void loop() {                 // core 1
     for (int8_t i = 0; i < LEGS_NUM; i++)
       if (legSolve[i].isSolved)
         setPosition(legs[i]);
-
   }
 }
-
-
-
-
 
 void serviceSetup() {
   serviceLoopIsReady = true;
 }
 
-void serviceLoop(void *parameter) {     // core 0
+void serviceLoop(void *parameter) {
   serviceSetup();
 
+  // Pierwsza pętla: Czekanie na połączenie i sekwencja wstawania
   while (1) {
     bool dataUpdated = BP32.update();
     if (dataUpdated) {
       standUp();
+      
+      // --- OPÓŹNIONA KALIBRACJA MPU ---
+#ifdef BAUD
+      Serial.println(F("Wstaje... Czekam 1.5s na pozycje docelowa."));
+#endif
+      // Usypiamy to zadanie na 1.5 sekundy, żeby serwa doszły do zera
+      vTaskDelay(1500 / portTICK_PERIOD_MS); 
+      
+#ifdef BAUD
+      Serial.println(F("Kalibruje idealny poziom. NIE RUSZAJ ROBOTA!"));
+#endif
+      mpu.calcOffsets(); // Zapisanie idealnego zera
+      mpuReady = true;   // Odblokowanie balansowania w pętli loop()
+#ifdef BAUD
+      Serial.println(F("Samopoziomowanie wlaczone!"));
+#endif
+      // --------------------------------
+
       break;
     }
   }
 
+  // Druga pętla: Regularne odczyty pada i generowanie chodu
   while (1) {
-
     bool dataUpdatedFromController = BP32.update();
+
     if (dataUpdatedFromController) {
       processControllers();
     }
 
-    gait.move();
-    gait.setDirection(WAIT);
 
+
+    static unsigned long lastTime = micros();
+    unsigned long now = micros();
+    float dt = (now - lastTime) / 1000000.0f;
+    lastTime = now;
+
+    gait.update(dt);
   }
 }
